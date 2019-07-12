7d559f8bfe8744a51cde71cb7acaa6e0fca4c788
avatar99 <51578188+avatar99@users.noreply.github.com>
2019-07-12 13:41:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['599159202895888405'];
+const devs = [''];
 const db = require('quick.db');
 const premium = ['557181389100023839']
 const client = new Discord.Client();   
